bd7b756d14763a98603e8859f54cc06a8173bdf3
Luciano Renovato de Albuquerque <luciano.ra@gmail.com>
2020-02-15 23:32:04 -0300

    Solucao para o exercicio 2.

---
+++ ExerciciosBasicosCSharp/Exercicio1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+namespace ExerciciosBasicosCSharp
+{
+    class Exercicio1
+    {
+        static Hashtable listaNumerosLetras = new Hashtable();
+        static Hashtable listaCometasCores = new Hashtable();
+
+        static void Exercicio1Solucao()
+        {
+            listaNumerosLetras.Add('A', 1);
+            listaNumerosLetras.Add('B', 2);
+            listaNumerosLetras.Add('C', 3);
+            listaNumerosLetras.Add('D', 4);
+            listaNumerosLetras.Add('E', 5);
+            listaNumerosLetras.Add('F', 6);
+            listaNumerosLetras.Add('G', 7);
+            listaNumerosLetras.Add('H', 8);
+            listaNumerosLetras.Add('I', 9);
+            listaNumerosLetras.Add('J', 10);
+            listaNumerosLetras.Add('K', 11);
+            listaNumerosLetras.Add('L', 12);
+            listaNumerosLetras.Add('M', 13);
+            listaNumerosLetras.Add('N', 14);
+            listaNumerosLetras.Add('O', 15);
+            listaNumerosLetras.Add('P', 16);
+            listaNumerosLetras.Add('Q', 17);
+            listaNumerosLetras.Add('R', 18);
+            listaNumerosLetras.Add('S', 19);
+            listaNumerosLetras.Add('T', 20);
+            listaNumerosLetras.Add('U', 21);
+            listaNumerosLetras.Add('V', 22);
+            listaNumerosLetras.Add('W', 23);
+            listaNumerosLetras.Add('X', 24);
+            listaNumerosLetras.Add('Y', 25);
+            listaNumerosLetras.Add('Z', 26);
+
+            listaCometasCores.Add("HALLEY", "AMARELO");
+            listaCometasCores.Add("ENCKE", "VERMELHO");
+            listaCometasCores.Add("WOLF", "PRETO");
+            listaCometasCores.Add("KUSHIDA", "AZUL");
+
+            // Se o resto da divisão do número do grupo por 45 for igual ao resto da divisão do número do cometa por 45, então o grupo será levado.
+            foreach (DictionaryEntry cometaCor in listaCometasCores)
+            {
+                int cometaResult = 1;
+                int cometaResto = -1;
+                int corResult = 1;
+                int corResto = -2;
+
+                foreach (char letraCometa in cometaCor.Key.ToString())
+                {
+                    cometaResult = cometaResult * Int32.Parse(listaNumerosLetras[letraCometa].ToString());
+                }
+                cometaResto = cometaResult % 45;
+
+                foreach (char letraCor in cometaCor.Value.ToString())
+                {
+                    corResult = corResult * Int32.Parse(listaNumerosLetras[letraCor].ToString());
+                }
+                corResto = corResult % 45;
+
+                if (cometaResto != corResto)
+                    Console.WriteLine("=====> NAO Serah levado: " + cometaCor.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExerciciosBasicosCSharp/Exercicio2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExerciciosBasicosCSharp
+{
+    static class Exercicio2
+    {
+        static int qtdEngenheiros = 75;
+        static List<string> sistemas = new List<string>();
+        static Hashtable vogais = new Hashtable();
+
+        public static void Exercicio2Solucao()
+        {
+            vogais.Add('A', 1);
+            vogais.Add('E', 2);
+            vogais.Add('I', 3);
+            vogais.Add('O', 5);
+            vogais.Add('U', 8);
+
+            sistemas.Add("SIRIUS");
+            sistemas.Add("LALANDE");
+            sistemas.Add("PROCION");
+            sistemas.Add("ALPHA CENTAURI");
+            sistemas.Add("BARNARD");
+
+            foreach (string sistema in sistemas)
+            {
+                int produtoVogais = 1;
+
+                foreach(char letra in sistema.ToString())
+                {
+                    if (vogais.Contains(letra))
+                    {
+                        produtoVogais = produtoVogais * Int32.Parse(vogais[letra].ToString());
+                    }
+                }
+
+                Console.WriteLine("Sistema: " + sistema + " quantidade: " + produtoVogais);
+                if (produtoVogais == qtdEngenheiros)
+                    Console.WriteLine("Destino: " + sistema);
+            }
+
+            Console.Read();
+        }
+    }
+}
